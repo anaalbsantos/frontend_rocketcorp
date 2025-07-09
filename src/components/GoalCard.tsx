f421278db934c99a77396f55e3c0d6592c788c97
anaalbsantos <6030.anaalbuquerque@gmail.com>
2025-07-09 16:06:51 -0300

    feat(goal): enhance GoalCard with edit and delete functionality, integrate dialog for new action plans

--- src/components/GoalCard.tsx
+++ src/components/GoalCard.tsx
@@ -1,5 +1,12 @@
 import { Progress } from "@/components/ui/progress";
-import { Pencil, Goal, TrashIcon } from "lucide-react";
+import {
+  Dialog,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { Pencil, Goal, TrashIcon, Check } from "lucide-react";
+import { DialogContent, DialogFooter, DialogHeader } from "./ui/dialog";
 
 interface GoalAction {
   description: string;
@@ -13,22 +20,51 @@ interface GoalData {
   actions: GoalAction[];
 }
 
-const GoalCard = ({ title, description, actions }: GoalData) => {
+interface GoalFunctions {
+  onEditGoal: () => void;
+  onDeleteGoal?: () => void;
+}
+
+const GoalCard = ({
+  title,
+  description,
+  actions,
+  onEditGoal,
+  onDeleteGoal,
+}: GoalData & GoalFunctions) => {
   const progress = Math.round(
     (actions.filter((a) => a.completed).length / actions.length) * 100
   );
   return (
     <div className="bg-white p-6 rounded-xl shadow-md flex flex-col gap-5">
-      <div className="flex items-start gap-4">
-        <Goal className="mt-1" />
-        <div className="flex flex-col">
-          <h2 className="gap-2 text-lg font-semibold">{title}</h2>
-          <p className="text-sm text-muted-foreground">{description}</p>
+      <div className="flex justify-between gap-4">
+        <div className="flex items-start gap-4">
+          <Goal className="mt-1" />
+          <div className="flex flex-col">
+            <h2 className="gap-2 text-lg font-semibold">{title}</h2>
+            <p className="text-sm text-muted-foreground">{description}</p>
+          </div>
+        </div>
+        <div>
+          <button
+            className="text-text-primary hover:underline text-xs px-2 py-1 rounded"
+            title="Editar"
+            onClick={onEditGoal}
+          >
+            <Pencil size={20} />
+          </button>
+          <button
+            className="text-red-600 hover:underline text-xs px-2 py-1 rounded"
+            title="Apagar"
+            onClick={onDeleteGoal}
+          >
+            <TrashIcon size={20} />
+          </button>
         </div>
       </div>
 
       <div className="flex items-center gap-1">
-        <h3 className="text-sm font-medium">{progress}%</h3>
+        <h3 className="text-sm font-medium">{progress || 0}%</h3>
         <Progress
           value={progress}
           className={"h-3 [&>div]:rounded-full [&>div]:bg-brand"}
@@ -57,36 +93,27 @@ const GoalCard = ({ title, description, actions }: GoalData) => {
                   <td className="px-3 py-2">{a.deadline}</td>
                   <td className="px-3 py-2 text-center">
                     <label className="inline-flex items-center cursor-pointer group">
-                      <input
-                        type="checkbox"
-                        // checked={acao.status === "concluida"}
-                        readOnly
-                        className="sr-only peer"
-                      />
-                      <span
+                      <button
+                        type="button"
                         className={
-                          `w-5 h-5 rounded border-2 flex items-center justify-center transition-colors duration-150 ` +
+                          `w-5 h-5 rounded border-2 flex items-center justify-center transition-colors duration-150 p-0 !important ` +
                           (a.completed
                             ? "bg-brand border-brand"
                             : "bg-white border-gray-300 group-hover:border-brand")
                         }
+                        aria-pressed={a.completed}
+                        title={
+                          a.completed ? "Concluída" : "Marcar como concluída"
+                        }
+                        // onClick={() => }
                       >
-                        {a.completed && (
-                          <svg
-                            className="w-3 h-3 text-white"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="3"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M5 13l4 4L19 7"
-                            />
-                          </svg>
-                        )}
-                      </span>
+                        <Check
+                          size={16}
+                          className={
+                            a.completed ? "text-white" : "text-transparent"
+                          }
+                        />
+                      </button>
                     </label>
                   </td>
                   <td className="px-3 py-2 text-center flex gap-2 justify-center">
@@ -109,9 +136,48 @@ const GoalCard = ({ title, description, actions }: GoalData) => {
           </table>
         </div>
         <div className="flex justify-end mt-4">
-          <button className="text-gray-600 text-xs hover:bg-muted">
-            Novo Plano de Ação
-          </button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <button className="text-gray-600 text-xs hover:bg-muted">
+                Novo Plano de Ação
+              </button>
+            </DialogTrigger>
+            <DialogContent>
+              <form className="flex flex-col gap-3">
+                <DialogHeader>
+                  <DialogTitle>Adicionar Plano de Ação</DialogTitle>
+                  <DialogDescription>
+                    Preencha os detalhes do novo plano de ação.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="w-full flex flex-col gap-1">
+                  <p>Título</p>
+                  <input
+                    maxLength={100}
+                    className="bg-white border rounded-md h-10 p-2 font-normal focus:outline-none focus:ring-1 focus:ring-brand"
+                    placeholder="Digite seu plano de ação"
+                  />
+                </div>
+                <div className="w-full flex flex-col gap-1">
+                  <p>Prazo</p>
+                  <input
+                    maxLength={250}
+                    className="bg-white border rounded-md h-10 p-2 font-normal focus:outline-none focus:ring-1 focus:ring-brand"
+                    placeholder="Digite uma descrição"
+                  />
+                </div>
+
+                <DialogFooter>
+                  <button
+                    type="submit"
+                    className="bg-brand text-white px-4 py-2 rounded-md"
+                  >
+                    Salvar
+                  </button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
--- src/pages/colaborador/Goals.tsx
+++ src/pages/colaborador/Goals.tsx
@@ -1,7 +1,99 @@
+import { useEffect, useState } from "react";
 import GoalCard from "@/components/GoalCard";
-import { PlusIcon } from "lucide-react";
+import api from "@/api/api";
+import { useUser } from "@/contexts/UserContext";
+import { type FieldValues, type SubmitHandler } from "react-hook-form";
+import GoalModal from "@/components/GoalModal";
+import toast from "react-hot-toast";
+
+interface GoalAction {
+  description: string;
+  deadline: string;
+  completed: boolean;
+}
+
+interface GoalData {
+  id: string;
+  title: string;
+  description: string;
+  actions: GoalAction[];
+}
+
+interface FormData extends FieldValues {
+  title: string;
+  description: string;
+}
 
 const Goals = () => {
+  const { userId } = useUser();
+  const [goals, setGoals] = useState<GoalData[]>([]);
+  const [open, setOpen] = useState(false);
+  const [selectedGoal, setSelectedGoal] = useState<GoalData | null>(null);
+
+  useEffect(() => {
+    const fetchGoals = async () => {
+      try {
+        const { data } = await api.get(`/goal/${userId}`);
+        setGoals(data);
+      } catch {
+        console.error("Erro ao buscar objetivos");
+      }
+    };
+
+    fetchGoals();
+  }, [userId]);
+
+  const handleNewGoal: SubmitHandler<FormData> = async (data: FormData) => {
+    try {
+      if (selectedGoal) {
+        // Editar objetivo existente
+        const response = await api.patch(`/goal/${selectedGoal.id}`, {
+          title: data.title,
+          description: data.description,
+        });
+        setGoals((prevGoals) =>
+          prevGoals.map((g) =>
+            g.id === selectedGoal.id ? { ...g, ...response.data } : g
+          )
+        );
+      } else {
+        // Novo objetivo
+        const response = await api.post(`/goal`, {
+          title: data.title,
+          description: data.description,
+          type: "PDI",
+        });
+        setGoals((prevGoals) => [...prevGoals, response.data]);
+      }
+      setOpen(false);
+      setSelectedGoal(null);
+
+      toast.success("Objetivo salvo com sucesso!");
+    } catch (e) {
+      toast.error(
+        "Erro ao salvar objetivo: " +
+          (e instanceof Error ? e.message : "Erro desconhecido")
+      );
+    } finally {
+      setOpen(false);
+      setSelectedGoal(null);
+    }
+  };
+
+  const handleDeleteGoal = async (goalId: string) => {
+    try {
+      await api.delete(`/goal/${goalId}`);
+      setGoals((prevGoals) => prevGoals.filter((g) => g.id !== goalId));
+
+      toast.success("Objetivo deletado com sucesso!");
+    } catch (error) {
+      toast.error(
+        "Erro ao deletar objetivo: " +
+          (error instanceof Error ? error.message : "Erro desconhecido")
+      );
+    }
+  };
+
   return (
     <div>
       <div className="bg-white p-6 border-b border-gray-200 shadow-sm">
@@ -10,34 +102,47 @@ const Goals = () => {
       <div className="flex flex-col p-6 gap-4">
         <div className="flex justify-between items-center">
           <h3 className="font-bold">Acompanhamento do PDI</h3>
-          <button className="bg-brand text-white text-sm flex gap-1 items-center">
-            <span>
-              <PlusIcon size={20} />
-            </span>
-            Novo Objetivo
-          </button>
+          <GoalModal
+            open={open}
+            onOpenChange={(v) => {
+              setOpen(v);
+              if (!v) setSelectedGoal(null);
+            }}
+            onSubmit={handleNewGoal}
+            initialValues={
+              selectedGoal
+                ? {
+                    title: selectedGoal.title,
+                    description: selectedGoal.description,
+                  }
+                : undefined
+            }
+          />
         </div>
-        <GoalCard
-          title="Melhorar habilidades de liderança"
-          description="Desenvolver competências para liderar projetos e equipes com mais eficiência."
-          actions={[
-            {
-              description: "Participar de curso de liderança",
-              completed: true,
-              deadline: "30/06/2024",
-            },
-            {
-              description: "Reuniões quinzenais com mentor",
-              completed: false,
-              deadline: "15/07/2024",
-            },
-            {
-              description: "Dar feedbacks construtivos para colegas",
-              completed: false,
-              deadline: "31/07/2024",
-            },
-          ]}
-        />
+        {goals.map((g) => (
+          <GoalCard
+            key={g.id}
+            title={g.title}
+            description={g.description}
+            actions={[
+              {
+                description: "Ações pendentes",
+                deadline: "31/12/2025",
+                completed: true,
+              },
+              {
+                description: "Estudar mais",
+                deadline: "31/12/2025",
+                completed: false,
+              },
+            ]}
+            onEditGoal={() => {
+              setSelectedGoal(g);
+              setOpen(true);
+            }}
+            onDeleteGoal={() => handleDeleteGoal(g.id)}
+          />
+        ))}
       </div>
     </div>
   );
