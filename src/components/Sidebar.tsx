9b58b21521d73c1bc3a959248138ed86f07dd78d
Ylson Santos <ylsonqueiroz@gmail.com>
2025-07-10 22:06:06 -0300

    fixação do header de critérios e aumento do zindex da sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -236,7 +236,7 @@ export const Sidebar = ({
           className="w-[232px] bg-white flex flex-col justify-between min-h-screen px-4 py-8"
           style={{
             boxShadow: "5px 0 15px -5px rgba(0, 0, 0, 0.12)",
-            zIndex: 50,
+            zIndex: 1050,
           }}
         >
           <div>
