5d0713448f58e91eff7f7baea26897d474c21c8a
anaalbsantos <6030.anaalbuquerque@gmail.com>
2025-07-13 19:21:21 -0300

    prod: simplify path resolution in Vite config by removing unnecessary imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
 });
