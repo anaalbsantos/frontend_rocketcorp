97bfce4122e4a95d424ed2a82dee35bfad7dab70
Ylson Santos <ylsonqueiroz@gmail.com>
2025-07-14 18:53:27 -0300

    correção nas responsividade de dashboard do gestor

--- src/pages/gestor/Dashboard.tsx
+++ src/pages/gestor/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react"; // <-- Importação React explícita
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Star, Users, FileText } from "lucide-react";
 
